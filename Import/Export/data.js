f2ff85e1fd3683f3d30f9c957e4443d389dc4c63
Qurat Ul Ain Shoro <65081454+QuratAin@users.noreply.github.com>
2022-11-22 20:47:05 +0500

    Create data.js

---
+++ Import/Export/data.js
@@ -0,0 +1,22 @@
+export const books = [
+    {
+        id : 1,
+        img: 'https://m.media-amazon.com/images/I/81OrrjDgaSL._AC_UL480_FMwebp_QL65_.jpg',
+        title: 'The Bookstore Sisters',
+        author: 'Alice Hoffman'
+    },
+
+    {
+        id : 2,
+        img: 'https://m.media-amazon.com/images/I/71PNGYHykrL._AC_UL480_FMwebp_QL65_.jpg',
+        title: 'It Starts with Us',
+        author: 'Colleen Hoover'
+    },
+
+    {
+        id : 3,
+        img: 'https://m.media-amazon.com/images/I/71PE4o0rSPL._AC_UL480_FMwebp_QL65_.jpg',
+        title: 'There is No Cloud',
+        author: 'Kat Wheeler'
+    },
+];
